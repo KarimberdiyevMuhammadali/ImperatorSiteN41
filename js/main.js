137782dc17498db52035492065e20e9d431749f5
bekzoddr <sodiqov.bekzoddr@gmail.com>
2023-12-15 01:00:56 +0500

    all changes

--- js/main.js
+++ js/main.js
@@ -15,3 +15,16 @@ function toggleBacktop() {
     backtop.classList.remove('backtop-show')
   }
 }
+window.onscroll = function() {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0 ) {
+    document.getElementById('navbar').classList.add('scrolled');
+  } else {
+    document.getElementById('navbar').classList.remove('scrolled');
+  }
+}
+const loading = document.getElementById("loader");
+
+
+setTimeout(() => {
+  loading.classList.add("loading-none");
+}, 1000);
